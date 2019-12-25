2a03c7fa1db56bf33f88e7ff01da577ba71b555e
msaeedha <mohamed.saeed-hammad@valeo.com>
2019-12-25 04:25:36 +0200

    create eclipse project, updated the code to measure timings and added video_input frame

--- ColorSpaceConversion.py
+++ ColorSpaceConversion.py
@@ -1,20 +1,22 @@
+'''
+Created on Dec 25, 2019
+
+@author: msaeedha
+'''
+import time
+
 import numpy as np
 import matplotlib.pyplot as plt
 import math
 from PIL import Image
-
-#raw_img =  np.fromfile('lena_color.tiff', dtype='uint8')
-#w = h = np.sqrt(raw_img[140:].shape[0]/3)
-#print ("Image size: ("+str(w)+", "+str(h)+")")
-#raw_img = np.reshape(raw_img[140:], (int(w), int(h), 3))
 
 def Convert_RGB_YUV(RGB_img):
     
     RGB_img = np.float32(np.array(RGB_img))
 
     RGB_YUV = np.float32(np.array([[0.299, 0.587, 0.114]
-                ,[-0.14713, -0.28886, 0.436]
-                ,[0.615, -0.51499, -0.10001]]))
+                                   ,[-0.14713, -0.28886, 0.436]
+                                   ,[0.615, -0.51499, -0.10001]]))
     
     YUV = np.dot(RGB_img, RGB_YUV.T)
 
@@ -35,6 +37,7 @@ def Convert_YUV_RGB(YUV_img, ROUND_FLAG = False):
     return Reconstructed_RGB
 
 def COLOR_Compression(YUV_img, mode = None):
+    start_time = time.time()
     if mode == '4:2:2':
         for i in range(0,YUV_img.shape[0]):
             for j in range(0, YUV_img.shape[1]):
@@ -164,23 +167,27 @@ def COLOR_Compression(YUV_img, mode = None):
                     YUV_img[i,j,2] = np.median( (YUV_img[i,j,2], YUV_img[i,j+1,2], YUV_img[i,j+2,2], YUV_img[i,j+3,2]) )
                 else:
                     YUV_img[i,j,2] = YUV_img[i,j-1,2]
+    print("{0}\t Compression Execution Time : {1} seconds".format(mode,(time.time() - start_time)))
     return YUV
 
 if __name__ == "__main__":
 
     mode_list = ['4:4:4','4:2:2', '4:2:0', '4:1:1', '4:4:0','4:2:0_Average', '4:2:0_Median', '4:2:2_Average', '4:1:1_Average', '4:1:1_Median']
-    img_list = ['lena_color.tiff', 'baboon.tiff', 'f16.tif', 'peppers.tif']
+    img_list = ['f0.bmp']
     fig, (ax1, ax2) = plt.subplots(2,2, figsize=(20,20))
     for image_name in img_list:
+        RGB_img = Image.open("video_input/"+image_name)
+        start_time=time.time()
+        YUV = Convert_RGB_YUV(RGB_img)
+        print("RGB->YUV444 Conversion Execution Time : {0} seconds".format((time.time() - start_time)))
         for Mode in mode_list:
             
-            RGB_img = Image.open(image_name)
+            Compressed_YUV = COLOR_Compression(YUV,Mode)
             
-            YUV = Convert_RGB_YUV(RGB_img)
+            start_time=time.time()
+            Reconstructed_RGB = Convert_YUV_RGB(Compressed_YUV, True)
+            print("Reconstructed_RGB Conversion Execution Time : {} seconds".format((time.time() - start_time)))
 
-            YUV = COLOR_Compression(YUV,Mode)
-
-            Reconstructed_RGB = Convert_YUV_RGB(YUV, True)
 
             MSE = np.mean( (RGB_img - Reconstructed_RGB)**2 )
 
@@ -190,14 +197,12 @@ if __name__ == "__main__":
                 PSNR = 20*math.log10(255.0/math.sqrt(MSE))
 
             diff_mat = RGB_img-Reconstructed_RGB
-            #diff_min = np.min(diff_mat)
-            #diff_range = np.max(diff_mat) - diff_min
-            #diff_mat = ((diff_mat - diff_min)/(diff_range))*255.0
+
             plt.ion()
             fig.suptitle('Color Compression mode: '+str(Mode)+', PSNR: '+str(PSNR)+' ,MSE: '+str(MSE), fontsize = 16)
             ax1[0].imshow(np.uint8(RGB_img))
             ax1[0].title.set_text('Original RGB image')
-            ax1[1].imshow(np.uint8(YUV))
+            ax1[1].imshow(np.uint8(Compressed_YUV))
             ax1[1].title.set_text('YUV Image')
             ax2[0].imshow(np.uint8(Reconstructed_RGB))
             ax2[0].title.set_text('Reconstructed RGB')
@@ -207,4 +212,4 @@ if __name__ == "__main__":
             plt.pause(0.05)
             nimage_name = image_name.split('.')[0]
             Mode = Mode.replace(':','_')
-            plt.savefig(str(Mode)+'_'+nimage_name+'.png', format='png')
+            plt.savefig("csc_output/"+str(Mode)+'_'+nimage_name+'.png', format='png')
